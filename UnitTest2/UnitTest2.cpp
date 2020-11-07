f6ede5efb98e14648ccd4940d545c9d842bdb5f6
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-11-07 22:40:45 +0200

    first master commit

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			const double x = 1;
+			const double y = 2;
+			t = g(x, y);
+			Assert::AreEqual(t,1,4);
+		}
+
+		};
+	};
